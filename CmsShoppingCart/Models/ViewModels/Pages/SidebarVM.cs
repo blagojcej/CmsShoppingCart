b43481bca68e83ede4d6e733b4ba3fbbe21b56ee
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-03-16 09:55:50 +0100

    Admin pages finished.

---
+++ CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using CmsShoppingCart.Models.Data;
+
+namespace CmsShoppingCart.Models.ViewModels.Pages
+{
+    public class SidebarVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Sidebar")]
+        [AllowHtml]
+        public string Body { get; set; }
+
+        public SidebarVM()
+        {
+            
+        }
+
+        public SidebarVM(SidebarDto row)
+        {
+            Id = row.Id;
+            Body = row.Body;
+        }
+    }
+}
